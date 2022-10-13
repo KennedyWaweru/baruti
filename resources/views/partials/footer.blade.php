7a4e12465f0de3d75b8ff9c522d934438c5b709b
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-13 15:55:45 +0300

    update copyright year

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-danger">&copy; 2021 KenWebShop Ltd</p>
+    <p class="col-md-4 mb-0 text-danger">&copy; 2022 KenWebShop Ltd</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-white text-decoration-none">
      logo img
