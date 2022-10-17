16723ab44a8bb754fd70e5693cf69b0267a677f1
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-17 23:40:45 +0300

    package image files from s3 bucket

--- resources/views/components/package-component.blade.php
+++ resources/views/components/package-component.blade.php
@@ -6,7 +6,7 @@
                 <div class="container">
                     <div class="row g-0">
                         {{-- @foreach($package_images as $package_image) --}}
-                            {!!env('AWS_BUCKET_URL').$package_images!!}
+                            {!!$package_images!!}
                         {{-- @endforeach --}}
                     </div>
                 </div>
