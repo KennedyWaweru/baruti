d6e5acb8f5e506ca9406a17d81ba78fa4bc4d5c1
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-17 23:56:22 +0300

    aws bucket url updated

--- resources/views/livewire/package-table.blade.php
+++ resources/views/livewire/package-table.blade.php
@@ -19,7 +19,8 @@
                 foreach($package_products as $product){
                     $products_array[]="<span class='badge bg-light text-dark'>$product->name</span>";
                     $products_stock[]=$product->stock;
-                    $product_image="<div class=\"col-sm-6\">\n<img src=\"$_ENV['AWS_BUCKET_URL'].$product->image_url\" alt=\"\" class=\"img-fluid rounded-start\">\n\t</div>";
+                    $aws_url = $_ENV['AWS_BUCKET_URL'].$product->image_url;
+                    $product_image="<div class=\"col-sm-6\">\n<img src=\"$aws_url\" alt=\"\" class=\"img-fluid rounded-start\">\n\t</div>";
                     $package_images[]=$product_image;
                 }
                 $package_products_html = implode(",",$products_array);
