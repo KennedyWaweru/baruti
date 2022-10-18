c123fb9fd6e60b452820281674172164ad6966af
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-18 22:01:25 +0300

    add js scripts for autocomplete

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -38,6 +38,8 @@
 @endsection
 
 @section('bottom_scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript"
         src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_API_KEY') }}&libraries=places" ></script>
 <script>
