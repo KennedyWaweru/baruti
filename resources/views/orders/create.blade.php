efbe8af4bca398452ae1d13b02cdbe89868a6bb9
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-18 22:11:49 +0300

    add js to autofill autocomplete fields

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -57,6 +57,14 @@
         function initialize() {
             var input = document.getElementById('autocomplete');
             var autocomplete = new google.maps.places.Autocomplete(input);
+
+			autocomplete.addListener('place_changed', function () {
+                var place = autocomplete.getPlace();
+                $('#latitude').val(place.geometry['location'].lat());
+                $('#longitude').val(place.geometry['location'].lng());
+                $("#latitudeArea").removeClass("d-none");
+                $("#longtitudeArea").removeClass("d-none");
+            });
         }
 </script>
 @endsection
