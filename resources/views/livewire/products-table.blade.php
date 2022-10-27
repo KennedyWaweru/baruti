3848c765dafa6079ae673937d8063fed2f71b7ba
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-27 18:38:30 +0300

    out of stock product display to admin

--- resources/views/livewire/products-table.blade.php
+++ resources/views/livewire/products-table.blade.php
@@ -14,7 +14,24 @@
                 {{Str::limit($product->description,20)}}
             </x-product-card>
         </div>
+        @else 
+        @auth
+            @can('admin')
+            <div class="col-md-4">
+                <x-product-card>
+                    <x-slot name="image_url"> {{$product->image_url}} </x-slot>
+                    <x-slot name="name"> {{$product->name}} </x-slot>
+                    <x-slot name="price">{{$product->price}} </x-slot>
+                    <x-slot name="slug">{{$product->slug}} </x-slot>
+                    <x-slot name="product_id">{{$product->id}}</x-slot>
+                    <x-slot name="stock">{{$product->stock}}</x-slot>
+                    {{Str::limit($product->description,20)}}
+                </x-product-card>
+            </div>
+            @endcan
+        @endauth
         @endif
+
         @endforeach
     </div>
 
