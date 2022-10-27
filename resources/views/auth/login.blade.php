b18eb33ee602dc85677700a3d7dd44b939c7b197
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-27 10:54:44 +0300

    add facebook login button on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,8 @@
                                 <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
                             </a>
 
-                            <a href="{{ route('facebook.login') }}" class="btn btn-facebook btn-user btn-block">
+                            {{-- <a href="{{ route('facebook.login') }}" class="btn btn-facebook btn-user btn-block"> --}}
+                                <a href="#" class="btn btn-facebook btn-user btn-block">
                                 <i class="fab fa-facebook-f fa-fw"></i>
                                 Login with Facebook
                              </a>
