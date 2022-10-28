4ca63cf2704a11481fe904bb9ae7ee6912fa935b
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-28 13:32:24 +0300

    vertical padding between vertical elements

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -59,7 +59,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-2">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
@@ -67,7 +67,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-2">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
