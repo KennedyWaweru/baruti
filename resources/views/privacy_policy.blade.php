8eea820c8115078186b4cf238b6181fc29ef36a3
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-28 16:21:13 +0300

    privacy policy text size

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -8,7 +8,7 @@
     <div class="container my-2">
         <div class="row bg-light">
             <h2>1. Whose privacy policy is this?</h2>
-            <p>
+            <p class="fs-3">
                 This Privacy Policy applies to the website accessible via <strong>https://baruti.co.ke</strong>
                 and the webshop that's accessible via the website, owned and operated by KenWebshop Ltd.
 
@@ -16,20 +16,20 @@
             </p>
             
             <h2>2. Information We Collect</h2>
-            <p>
+            <p class="fs-3">
                 When you register for a Baruti.co.ke account, and when you contact us for customer support, we collect the following personal data from you:
                 <strong>Personal Information:</strong> such as, your name (first and last names), email address and location;
             </p>
             
             <h2>3. How We Use Information</h2>
-            <p>
+            <p class="fs-3">
                 To ensure we provide you with delivery services, we have to collect some personal data, names and address. All of this is laid out in this section.
 
                 <b>With your consent</b> we provide you with marketing information about products and services via email and SMS which we feel may interest you;
             </p>
 
             <h2>4. Information We Share</h2>
-            <p>We do not intend to share your data with any third party.</p>
+            <p class="fs-3">We do not intend to share your data with any third party.</p>
 
         </div>
     </div>
