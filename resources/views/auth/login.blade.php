361ba72748010aa1e507c8d9201c2970c34d8bb0
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-28 13:00:14 +0300

    put buttons in one row

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,14 +69,16 @@
                             </div>
                         </div>
 
-                            
-                                <a href="{{ url('authorized/google') }}">
-                                    <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
-                                </a>    
-                           
-                            
-                                <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
-                            
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a href="{{ url('authorized/google') }}">
+                                        <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                                    </a>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+                                </div>
+                            </div>
                             
                     </form>
                 </div>
