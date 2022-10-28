2dd1346a00b602dc9e4e83f42f87059e106d54c9
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-10-28 13:30:40 +0300

    added social authentication buttons to register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('fb_login_script')
+    {{-- Login with facebook script --}}
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v15.0" nonce="WomcZGRo"></script>
+@endsection
 
 @section('content')
-<div class="container">
+<div class="container my-3">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -11,7 +17,7 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="form-group row my-2">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
@@ -25,7 +31,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-2">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -39,7 +45,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-2">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -66,6 +72,25 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
+                            </div>
+                        </div>
+
+                        
+                        <hr class="my-2 text-success">
+
+                        {{-- Social login --}}
+
+                        <div class="row mb-2">
+                            <div class="col-md-6">
+                                <a href="{{ url('authorized/google') }}">
+                                    <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                                </a>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="#">
+                                    <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+                                </a>
+    
                             </div>
                         </div>
                     </form>
