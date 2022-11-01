89e17575076b28c5e9cdbe4f6fd1f14a71ae8e13
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-01 14:28:22 +0300

    limit number of products on initial load to 6

--- app/Http/Livewire/ProductsTable.php
+++ app/Http/Livewire/ProductsTable.php
@@ -16,8 +16,8 @@ class ProductsTable extends Component
     
     //public $listeners = ['moreProducts'];
     public function mount(){
-        $this->products = Firework::all();
-        //$this->products = DB::table('fireworks')->limit($this->cursor)->get();
+        //$this->products = Firework::all();
+        $this->products = DB::table('fireworks')->limit($this->cursor)->get();
         foreach($this->products as $product){
             $this->quantity[$product->id] = 1;
             $this->stock[$product->id] = $product->stock;
