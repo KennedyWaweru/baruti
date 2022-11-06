212e1e79f52e6fcef8c8a2c0ecad535086b06bd6
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-06 08:59:49 +0300

    dump die the paginated products after limit

--- app/Http/Livewire/ProductsTable.php
+++ app/Http/Livewire/ProductsTable.php
@@ -18,7 +18,7 @@ class ProductsTable extends Component
     public function mount(){
         //$this->products = Firework::all();
         //$this->products = DB::table('fireworks')->limit($this->cursor)->get();
-        $this->products = Firework::forPage(1,4)->collect();
+        $this->products = Firework::forPage(1,4);
         dd($this->products);
         
         foreach($this->products as $product){
