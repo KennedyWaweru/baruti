e0e39c62063424aaa1340ca049cf32fe73701628
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-06 12:30:34 +0300

    js files added via npm

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kenwaweru/Sites/kenshop/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kenwaweru/Sites/kenshop/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Acer\Documents\Sites\baruti\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Acer\Documents\Sites\baruti\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
