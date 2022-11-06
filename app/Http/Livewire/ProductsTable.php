f9c07ae90ed3fab3472bf596555823987826212d
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-06 08:50:28 +0300

    dump die the products after limit

--- app/Http/Livewire/ProductsTable.php
+++ app/Http/Livewire/ProductsTable.php
@@ -16,10 +16,11 @@ class ProductsTable extends Component
     
     //public $listeners = ['moreProducts'];
     public function mount(){
-        $this->products = Firework::all();
+        //$this->products = Firework::all();
+        $this->products = DB::table('fireworks')->limit($this->cursor)->get();
         //$this->products = Firework::forPage(1,4)->collect()->all();
         dd($this->products);
-        //$this->products = DB::table('fireworks')->limit($this->cursor)->get();
+        
         foreach($this->products as $product){
             $this->quantity[$product->id] = 1;
             $this->stock[$product->id] = $product->stock;
