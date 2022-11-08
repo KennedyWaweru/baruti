7f1872b558b7dd5ed06eee81880ef1efa2661523
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-08 13:20:12 +0300

    append paginated data on the existing array

--- app/Http/Livewire/ProductsTable.php
+++ app/Http/Livewire/ProductsTable.php
@@ -18,7 +18,7 @@ class ProductsTable extends Component
     //public $listeners = ['moreProducts'];
     public function mount(){
         //$this->products = Firework::all();
-        $this->products = Firework::forPage($this->current_page,$this->products_on_page)->get();
+        $this->products[] = Firework::forPage($this->current_page,$this->products_on_page)->get();
         dd($this->products);
         
         foreach($this->products as $product){
