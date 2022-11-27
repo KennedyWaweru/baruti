14b6a671068ef5dfb84aeeab7ff5f79fbd49bdbb
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-11-27 10:05:12 +0300

    Use flex divs to show countdown

--- resources/views/fireworks/dashboard.blade.php
+++ resources/views/fireworks/dashboard.blade.php
@@ -5,6 +5,12 @@
 		<div id="heading-text">
 			<div class="p-4 w-100 h-100 rounded-3 border bg-dark">
 				<h1 id="countdown"></h1>
+				<div class="d-flex flex-row justify-content-evenly bd-highlight mb-3">
+					<div class="p2 bd-highlight">Flex Item</div>
+					<div class="p2 bd-highlight">Flex Item</div>
+					<div class="p2 bd-highlight">Flex Item</div>
+					<div class="p2 bd-highlight">Flex Item</div>
+				</div>
 				<h1>Happy New Year</h1>
 			</div>
 			
