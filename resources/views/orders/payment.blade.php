e4ae1dc0eff7505186a79a091548714d824eee1b
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-12-02 17:34:25 +0300

    use if instead of isset to teest truthyness of variable

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -30,12 +30,12 @@
             @endisset
         </div>
         <div class="col-md-5 col-sm-12 bg-success text-white p-3">
-            @isset($buyNow)
+            @if($buyNow)
                 <h2 class="text-center fs-3 text-dark">Buy Now</h2>
                 @livewire('buy-now-summary',['item_slug'=>$itemSlug])
             @else
                 @livewire('cart-summary')
-            @endisset
+            @endif
         </div>
 
     </div>
