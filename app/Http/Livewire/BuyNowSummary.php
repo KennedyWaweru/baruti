a118e7209401ef52b45c6318262de12a9a09b863
Waweru Kennedy <kennedywaweru17@gmail.com>
2022-12-02 15:26:34 +0300

    use package from db

--- app/Http/Livewire/BuyNowSummary.php
+++ app/Http/Livewire/BuyNowSummary.php
@@ -9,6 +9,9 @@ use App\Package;
 class BuyNowSummary extends Component
 {
     public $package;
+    public function mount(){
+        $this->$package = Package::findOrFail($package);
+    }
     public function render()
     {
         return view('livewire.buy-now-summary');
