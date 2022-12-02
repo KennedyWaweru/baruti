669dfbdfbbb156850e0484040f7b771e1f5173a1
Kennedy Waweru <kennedywaweru17@gmail.com>
2022-12-02 12:17:27 +0000

    generated livewire files to show summary of buy now items

---
+++ app/Http/Livewire/BuyNowSummary.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BuyNowSummary extends Component
+{
+    public function render()
+    {
+        return view('livewire.buy-now-summary');
+    }
+}
